5fde1280ccc9e26ef04589b2922149374c67ae22
liukaixin <15713857919@163.com>
2024-09-29 16:40:25 +0800

    setState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import EventApp from "./components/Event";
 import UseStateApp from "./components/useState";
+import SetStateApp from "./components/setState";
 
 function App() {
   return (
     <div>
       {/* <EventApp /> */}
-      <UseStateApp />
+      {/* <UseStateApp /> */}
+      <SetStateApp />
     </div>
   );
 }
