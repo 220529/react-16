402e3bccf46c200eae4f804221d0b676c9727785
liukaixin <15713857919@163.com>
2024-10-12 22:19:26 +0800

    batchedUpdates

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import EventApp from "./components/Event";
 import UseStateApp from "./components/useState";
 import SetStateApp from "./components/setState";
+import BatchedUpdatesApp from "./components/batchedUpdates";
 
 function App() {
   return (
     <div>
       {/* <EventApp /> */}
       {/* <UseStateApp /> */}
-      <SetStateApp />
+      {/* <SetStateApp /> */}
+      <BatchedUpdatesApp />
     </div>
   );
 }
